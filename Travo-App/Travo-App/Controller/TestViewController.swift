c4de13758224779be1719358e56bff44034bf07d
Sogyal <s3692351@student.rmit.edu.au>
2019-09-08 10:16:43 +1000

    Few Updates to HomeView and Constraint Fixes

---
+++ Travo-App/Travo-App/Controller/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  Travo-App
+//
+//  Created by Sogyal Thundup Sherpa on 4/9/19.
+//  Copyright © 2019 Sogyal Thundup Sherpa. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
