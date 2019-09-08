21db6a6055572da3a931365e41fe5015a59cfad4
Sogyal <s3692351@student.rmit.edu.au>
2019-09-08 11:46:14 +1000

    Added icons

---
+++ Travo-App/Travo-App/Controller/Test1ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Test1ViewController.swift
+//  Travo-App
+//
+//  Created by Sogyal Thundup Sherpa on 8/9/19.
+//  Copyright © 2019 Sogyal Thundup Sherpa. All rights reserved.
+//
+
+import UIKit
+
+class Test1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
