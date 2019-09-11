45c076e0da86c95681b81886733cbc8f2dad97d6
Gayuru <ggayurugunawardana@gmail.com>
2019-09-11 21:35:30 +1000

    Added new classes

---
+++ Travo-App/Travo-App/Model/Place.swift
@@ -0,0 +1,20 @@
+//
+//  Places.swift
+//  Travo-App
+//
+//  Created by Sogyal Thundup Sherpa on 10/9/19.
+//  Copyright © 2019 Sogyal Thundup Sherpa. All rights reserved.
+//
+
+import Foundation
+
+struct Place{
+    private let name:String
+    private let location:String
+    private let image:String
+    private let starRating:Int
+    private let openTimes:String
+    private let weather:Weather
+    
+    
+}
