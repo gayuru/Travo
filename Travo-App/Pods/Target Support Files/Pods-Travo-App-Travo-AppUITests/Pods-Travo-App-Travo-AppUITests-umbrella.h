085f6b4353a9d09bb1870f3b4287f65aa573b91c
Gayuru <ggayurugunawardana@gmail.com>
2019-09-21 13:11:20 +1000

    Tests for Dashboard done | fixed cocoapods for testing

--- Travo-App/Pods/Target Support Files/Pods-Travo-App-Travo-AppUITests/Pods-Travo-App-Travo-AppUITests-umbrella.h
+++ Travo-App/Pods/Target Support Files/Pods-Travo-App-Travo-AppUITests/Pods-Travo-App-Travo-AppUITests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_Travo_AppUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Travo_AppUITestsVersionString[];
+FOUNDATION_EXPORT double Pods_Travo_App_Travo_AppUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Travo_App_Travo_AppUITestsVersionString[];
 
